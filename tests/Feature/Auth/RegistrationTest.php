b236aa493fb8372194344bb14f7575508942105c
Nicholas Avenell <nicholas@learningonscreen.ac.uk>
2026-03-06 22:28:48 +0000

    Add feature check for registration in tests to skip if disabled

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,12 +1,18 @@
 <?php
 
 test('registration screen can be rendered', function () {
+    if (!config('fortify.features') || !in_array(Laravel\Fortify\Features::registration(), config('fortify.features'))) {
+        $this->markTestSkipped('Registration is disabled.');
+    }
     $response = $this->get(route('register'));
 
     $response->assertOk();
 });
 
 test('new users can register', function () {
+    if (!config('fortify.features') || !in_array(Laravel\Fortify\Features::registration(), config('fortify.features'))) {
+        $this->markTestSkipped('Registration is disabled.');
+    }
     $response = $this->post(route('register.store'), [
         'name' => 'John Doe',
         'email' => 'test@example.com',
