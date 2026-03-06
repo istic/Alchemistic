6f7ba5bd9d787dd4dc0f5b2a1fba9cd5d0dd6a8f
Nicholas Avenell <nicholas@learningonscreen.ac.uk>
2026-03-06 21:31:42 +0000

    Update application name to Alchemistic in environment and header files

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -3,13 +3,13 @@
 ])
 
 @if($sidebar)
-<flux:sidebar.brand name="Istic Hosting" {{ $attributes }}>
+<flux:sidebar.brand name="Alchemistic" {{ $attributes }}>
     <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
         <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
     </x-slot>
 </flux:sidebar.brand>
 @else
-<flux:brand name="Istic Hosting" {{ $attributes }}>
+<flux:brand name="Alchemistic" {{ $attributes }}>
     <x-slot name="logo" class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
         <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
     </x-slot>
